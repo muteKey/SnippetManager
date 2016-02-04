3d1f1f58a06149481768d34870bbbfc56a2f06d0
Kirill Ushkov <kirillushkov2010@gmail.com>
2016-02-04 04:18:43 +0200

    added loading snippets in UI

--- SnippetManager/ViewController.m
+++ SnippetManager/ViewController.m
@@ -24,30 +24,54 @@
     
     __weak typeof (self) wSelf = self;
     
-//    [[ApiClient sharedInstance] snippetsWithSuccess:^(NSURLSessionDataTask *task, id responseObject) {
-//        
-//        for (NSDictionary *dict in responseObject) {
-//            NSString *identifier = dict[@"_id"];
-//            
-//            Snippet *snippet = (Snippet*)[wSelf.dataController findOrCreateObjectWithEntityName:@"Snippet"
-//                                                                                      predicate:[NSPredicate predicateWithFormat:@"snippetIdentifier == %@", identifier]
-//                                                                              createNewIfAbsent:YES];
-//            
-//            snippet.snippetIdentifier = identifier;
-//            snippet.snippetDescription = dict[@"description"];
-//            snippet.snippetShortcut = dict[@"shortcut"];
-//            snippet.snippetTitle = dict[@"title"];
-//        }
-//        
-//        [wSelf.dataController.managedObjectContext save:nil];
-//        
-//    } failure:^(NSURLSessionDataTask *task, NSError *error) {
-//        
-//        NSLog(@"error %@", error);
-//
-//    }];
-    
-    [[ApiClient sharedInstance] mediaWithSuccess:nil failure:nil];
+    [[ApiClient sharedInstance] snippetsWithSuccess:^(NSURLSessionDataTask *task, id responseObject) {
+        
+        for (NSDictionary *dict in responseObject) {
+            NSString *identifier = dict[@"_id"];
+            
+            Snippet *snippet = (Snippet*)[wSelf.dataController findOrCreateObjectWithEntityName:@"Snippet"
+                                                                                      predicate:[NSPredicate predicateWithFormat:@"snippetIdentifier == %@", identifier]
+                                                                              createNewIfAbsent:YES];
+            
+            snippet.snippetIdentifier = identifier;
+            snippet.snippetDescription = dict[@"description"];
+            snippet.snippetShortcut = dict[@"shortcut"];
+            snippet.snippetTitle = dict[@"title"];
+            
+            NSArray *fileIDs = dict[@"snippetFile"];
+            
+            NSString *fileID = [fileIDs firstObject];
+            
+            [[ApiClient sharedInstance] mediaWithID:fileID
+                                            success:^(NSURLSessionDataTask *task, id responseObject) {
+                                                
+                                                NSXMLDocument *doc = (NSXMLDocument *)responseObject;
+                                                NSData *data = doc.XMLData;
+                                                
+                                                NSString *name = [NSString stringWithFormat:@"%@.codesnippet", snippet.snippetIdentifier];
+                                                
+                                                NSError *er = nil;
+                                                
+                                                [data writeToFile:name
+                                                          options:NSDataWritingAtomic
+                                                            error:&er];
+                                                
+                                                NSLog(@"%@", er.localizedDescription);
+                                                
+                                            }
+                                            failure:^(NSURLSessionDataTask *task, NSError *error) {
+                                                NSLog(@"Error %@", error);
+                                            }];
+        }
+        
+        [wSelf.dataController.managedObjectContext save:nil];
+        
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        
+        NSLog(@"error %@", error);
+
+    }];
 }
 
 - (DataController *)dataController {
